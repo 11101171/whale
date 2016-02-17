d4cbe7546e88c25d8b0aa02c0bdae93a6ebe8fd5
曾宁忠 <zengningzhong@souche.com>
2016-02-17 16:12:38 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+//****************************************************/
+// https://github.com/11101171
+// Author: ningzhong.zeng
+//****************************************************/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Start Main func()")
+}
